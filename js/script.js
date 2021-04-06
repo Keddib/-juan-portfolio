c47115edbf3053cd860df0f9ce4e16a1bdf70f20
khalid <keddib@student.1337.ma>
2021-04-06 12:12:57 +0100

    animation done

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ const sections = document.querySelectorAll('.animate');
 observer = new IntersectionObserver((entries) => {
 
 	entries.forEach((entry) => {
-
+		console.log(entry);
 		if(entry.intersectionRatio > 0) {
-			entry.target.style.animation = `anim1 1s forwards ease-out`;
+			entry.target.style.animation = `anim1 2s forwards ease-out`;
 		}
 		else{
 			entry.target.style.animation = 'none';
